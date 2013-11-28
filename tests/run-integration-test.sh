98358c6a6abfba39db8c0e470b950a9b641183a2
Santeri Paavolainen <santtu@iki.fi>
2013-11-28 23:14:21 +0200

    Full integration test runner script, does CFN deploy, runs software, runs tests, tears down CFN stack.

---
+++ tests/run-integration-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -e
+cur_dir=$(dirname $0)
+top_dir=$(dirname $0)/..
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 AWS-ACCESS-KEY AWS-SECRET-KEY [KEY-NAME [REGION]] " >&2
+    exit 2
+fi
+access_key="$1"
+secret_key="$2"
+key_name="${3-}"
+region="${4-us-east-1}"
+
+export AWS_DEFAULT_REGION=$region
+export AWS_ACCESS_KEY_ID=$access_key
+export AWS_SECRET_ACCESS_KEY=$secret_key
+
+(cd $top_dir && ./deploy stop ; ./deploy)
+if ./freeze-thaw-aws.sh "$access_key" "$secret_key" "$key_name" "$region"; then
+    retval=0
+else
+    retval=1
+fi
+(cd $top_dir && ./deploy stop)
+exit $retval
