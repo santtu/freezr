0ba575650dca674b02cbd864b9102d9f79ce8a96
Santeri Paavolainen <santtu@iki.fi>
2013-11-29 21:54:47 +0200

    Added minimal setup.py, updated gitignore to ignore generated build files.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='freezr',
+      version='0.1.0',
+      description='Web service for starting and stopping projects in AWS',
+      url='http://github.com/santtu/freezr',
+      author='Santeri Paavolainen',
+      author_email='santtu@iki.fi',
+      license='GPLv3',
+      packages=['freezr'],
+      zip_safe=False)
